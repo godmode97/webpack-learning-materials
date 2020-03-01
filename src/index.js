e5d4e6ad83a357aeeab7f54d19e9edd8d5877124
Neil Monzales <neil.monzales@whitecloak.com>
2020-03-01 19:05:48 +0800

    ui changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import "./assets/styles/style.css"
+import logo from "./assets/images/react.png"
 
 function App(){
   return (
     <div>
       <h1>Hello World From React!!!</h1>
+      <img src={logo} alt="React" />
     </div>
   )
 }
